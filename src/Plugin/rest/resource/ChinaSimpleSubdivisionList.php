dd35fef92081b4f03219131b086e73adfae716ae
kentkent <kent@solody.com>
2018-08-03 21:32:49 +0800

    fix address api

--- src/Plugin/rest/resource/ChinaSimpleSubdivisionList.php
+++ src/Plugin/rest/resource/ChinaSimpleSubdivisionList.php
@@ -21,121 +21,124 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
  *   }
  * )
  */
-class ChinaSimpleSubdivisionList extends ResourceBase
-{
+class ChinaSimpleSubdivisionList extends ResourceBase {
 
-    /**
-     * A current user instance.
-     *
-     * @var \Drupal\Core\Session\AccountProxyInterface
-     */
-    protected $currentUser;
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
 
 
-    /**
-     * @var SubdivisionRepositoryInterface
-     */
-    protected $subdivisionRepository;
+  /**
+   * @var SubdivisionRepositoryInterface
+   */
+  protected $subdivisionRepository;
 
-    /**
-     * Constructs a new ChinaSimpleSubdivisionList object.
-     *
-     * @param array $configuration
-     *   A configuration array containing information about the plugin instance.
-     * @param string $plugin_id
-     *   The plugin_id for the plugin instance.
-     * @param mixed $plugin_definition
-     *   The plugin implementation definition.
-     * @param array $serializer_formats
-     *   The available serialization formats.
-     * @param \Psr\Log\LoggerInterface $logger
-     *   A logger instance.
-     * @param \Drupal\Core\Session\AccountProxyInterface $current_user
-     *   A current user instance.
-     */
-    public function __construct(
-        array $configuration,
-        $plugin_id,
-        $plugin_definition,
-        array $serializer_formats,
-        LoggerInterface $logger,
-        AccountProxyInterface $current_user,
-        SubdivisionRepositoryInterface $subdivision_repository)
-    {
-        parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+  /**
+   * Constructs a new ChinaSimpleSubdivisionList object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user,
+    SubdivisionRepositoryInterface $subdivision_repository) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
 
-        $this->currentUser = $current_user;
-        $this->subdivisionRepository = $subdivision_repository;
+    $this->currentUser = $current_user;
+    $this->subdivisionRepository = $subdivision_repository;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('address_api'),
+      $container->get('current_user'),
+      $container->get('address_api.iso_code_order_subdivision_repository')
+    );
+  }
+
+  /**
+   * Responds to GET requests.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   The HTTP response object.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   *   Throws exception expected.
+   */
+  public function get() {
+
+    // You must to implement the logic of your REST Resource here.
+    // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
-    {
-        return new static(
-            $configuration,
-            $plugin_id,
-            $plugin_definition,
-            $container->getParameter('serializer.formats'),
-            $container->get('logger.factory')->get('address_api'),
-            $container->get('current_user'),
-            $container->get('address_api.iso_code_order_subdivision_repository')
-        );
+    // 生成数据
+    $data = [];
+    $provinces = $this->subdivisionRepository->getList(['CN'], 'zh-hans');
+    foreach ($provinces as $province_code => $province_name) {
+      $data[] = [
+        'name' => $province_name,
+        'value' => $province_code
+      ];
     }
 
-    /**
-     * Responds to GET requests.
-     *
-     * @return \Drupal\rest\ResourceResponse
-     *   The HTTP response object.
-     *
-     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
-     *   Throws exception expected.
-     */
-    public function get()
-    {
+    foreach ($provinces as $province_code => $province_name) {
+      $cities = $this->subdivisionRepository->getList(['CN', $province_code], 'zh-hans');
 
-        // You must to implement the logic of your REST Resource here.
-        // Use current user after pass authentication to validate access.
-        if (!$this->currentUser->hasPermission('access content')) {
-            throw new AccessDeniedHttpException();
-        }
+      foreach ($cities as $city_code => $city_name) {
+        $data[] = [
+          'name' => $city_name,
+          'value' => $city_code,
+          'parent' => $province_code
+        ];
 
-        // 生成数据
-        $data = [];
-        $provinces = $this->subdivisionRepository->getList(['CN'], 'zh-hans');
-        foreach ($provinces as $province_code => $province_name) {
+        $districts = $this->subdivisionRepository->getList(['CN', $province_code, $city_code], 'zh-hans');
+        if (count($districts)) {
+          foreach ($districts as $district_code => $district_name) {
             $data[] = [
-                'name' => $province_name,
-                'value' => $province_code
+              'name' => $district_name,
+              'value' => $district_code,
+              'parent' => $city_code
             ];
+          }
+        } else {
+          $data[] = [
+            'name' => '',
+            'value' => '',
+            'parent' => $city_code
+          ];
         }
-
-        foreach ($provinces as $province_code => $province_name) {
-            $cities = $this->subdivisionRepository->getList(['CN', $province_code], 'zh-hans');
-
-            foreach ($cities as $city_code => $city_name) {
-                $data[] = [
-                    'name' => $city_name,
-                    'value' => $city_code,
-                    'parent' => $province_code
-                ];
-
-                $districts = $this->subdivisionRepository->getList(['CN', $province_code, $city_code], 'zh-hans');
-
-                foreach ($districts as $district_code => $district_name) {
-                    $data[] = [
-                        'name' => $district_name,
-                        'value' => $district_code,
-                        'parent' => $city_code
-                    ];
-                }
-            }
-        }
-
-        $response = new ResourceResponse($data, 200);
-        return $response;
+      }
     }
+
+    $response = new ResourceResponse($data, 200);
+    return $response;
+  }
 
 }
